7a6f4533994e0789d8f49b369c1ba6cdb4a9f92f
marcelbaduaballistix <44352240+marcelbaduaballistix@users.noreply.github.com>
2019-10-10 10:16:22 +0800

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -111,8 +111,9 @@ fi
 
 echo "Installing Ballstix SPE custom themes"
 cd ${VVV_PATH_TO_SITE}/public_html/wp-content/themes
-git clone https://github.com/marcelbaduaballistix/_dash.git _dash
-git clone https://github.com/marcelbaduaballistix/_ballistix.git _ballistix
+
+git clone https://github.com/ballistix-dev/_dash.git _dash
+git clone https://github.com/ballistix-dev/_ballistix.git _ballistix
 noroot wp theme activate _ballistix
 
 echo "Site Template provisioner script completed"
