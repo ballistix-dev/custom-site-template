03d6f6098b4b525f91b1964a9f22c7c1336f8364
Marcel Badua <bitterdash@gmail.com>
2019-10-10 11:26:06 +0800

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -112,7 +112,7 @@ fi
 echo "Installing Ballstix SPE custom themes"
 cd ${VVV_PATH_TO_SITE}/public_html/wp-content/themes
 
-git clone https://github.com/ballistix-dev/_dash.git _dash
+git clone https://github.com/marcelbadua/_dash.git _dash
 git clone https://github.com/ballistix-dev/_ballistix.git _ballistix
 noroot wp theme activate _ballistix
 
